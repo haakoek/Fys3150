c451dfb1ae5fbc89daf80fbe7a008f95ef2033b8
hakii27 <haakoek@student.matnat.uio.no>
2014-09-08 14:39:54 +0200

    Further comments added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+ Program to solve the 1-dimensional Poisson equation
+ with Dirichlet boundary conditions.
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -10,11 +15,15 @@
 using namespace std;
 using namespace arma;
 
+//Function declarations
+
 double source_term(double);
 double exact_solution(double);
 void solver(double*,double*,double*,double*,double*,int);
 void rel_error(double*,double*,double*,int);
 void output(double*, double*,double*, int);
+
+//Two output files may be needed, and are declared as global variables
 
 ofstream ofile1;
 ofstream ofile2;
@@ -25,12 +34,24 @@ int main(int argc, char* argv[])
 
     int i,n;
     double *a, *b, *c, *g, *computed_solution,*exact,*relative_error;
-    ofile1.open("output1.txt", ios::out);
-    ofile2.open("output2.txt");
+    char *outfile1;
+    char *outfile2;
 
-    //Read problem size from command line
-    n = atoi(argv[1]);
-    cout << n << endl;
+
+
+    //Read problem size from command line, and abort if there are too few command-line arguments.
+
+    if(argc <= 2) {
+        cout << "Bad usage: " << argv[0] << endl;
+        cout << "Read problem size, " << endl;
+        cout << "and at least one output file on command line" << endl;
+        exit(1);
+    } else {
+        n = atoi(argv[1]);
+        outfile1 = argv[2];
+        ofile1.open(outfile1, ios::out);
+        cout << n << endl;
+    }
 
     //Initialize variables
 
@@ -53,13 +74,13 @@ int main(int argc, char* argv[])
         exact[i] = exact_solution(i*h);
     }
 
-    //Set boundary conditions for the computed solution
+    //Set boundary conditions for the computed solution (Dirichlet).
 
     computed_solution[0] = 0.0;
     computed_solution[n+1] = 0.0;
 
     //Take time of the solver function, compute the solution, and extract the relative error,
-    //and write to file.
+    //and write the relative error at n/2, and the execution time to outfile1.
 
     clock_t begin1 = clock();
 
@@ -70,6 +91,8 @@ int main(int argc, char* argv[])
 
     rel_error(relative_error, exact, computed_solution,n);
 
+    //cout << relative_error[n/2] << endl;
+
     ofile1 << setiosflags(ios::showpoint | ios::uppercase);
     ofile1 << setw(15) << setprecision(8) << relative_error[n/2];
     ofile1 << setw(15) << setprecision(8) << elapsed_secs1;
@@ -79,12 +102,14 @@ int main(int argc, char* argv[])
 
     if(n <= 10000) {
 
+        //Declare matrices, using armadillo.
+
         mat L = zeros<mat>(n,n);
         mat U = zeros<mat>(n,n);
         mat P = zeros<mat>(n,n);
         mat A = zeros<mat>(n,n);
         mat B = zeros<mat>(n,1);
-        //mat C = randu<mat>(n,n);
+        mat C = randu<mat>(n,n);
 
         for(i=0; i < n; i++) {
             A(i,i) = 2.0;
@@ -94,22 +119,32 @@ int main(int argc, char* argv[])
                 A(i+1,i) = -1.0;
             }
         }
-        /*
-        for(i=0; i < n; i++) {
-            B(i,0) = g[i+1];
-        }
-        */
-
-        clock_t begin2 = clock();
 
         //Standard Gauss-Jordan elimination
 
-        mat X = solve(A,B);
+        clock_t begin2 = clock();
 
+        mat X = solve(A,B);
 
         clock_t end2 = clock();
 
         double elapsed_secs2 = double(end2 - begin2) / CLOCKS_PER_SEC;
+
+        /*This second Gauss solution is of no significance to the actual problem,
+          but to test the exectuion time, as it seems that armadillo recognizes
+          the tridiagonal property of A, and reduces the exection time for GJ, from
+          O(n**3) to O(n**2). Hence we want to check that for a random generated matrix C, the
+          solution is actually O(n**3) for GJ as we would expect.
+        */
+
+
+        clock_t begin4 = clock();
+
+        mat X2 = solve(C,B);
+
+        clock_t end4 = clock();
+
+        double elapsed_secs4 = double(end4 - begin4) / CLOCKS_PER_SEC;
 
         //LU decomposition
 
@@ -125,17 +160,21 @@ int main(int argc, char* argv[])
 
         double elapsed_secs3 = double(end3 - begin3) / CLOCKS_PER_SEC;
 
-        //Output
+        //Write execution times to file.
 
         ofile1 << setw(15) << setprecision(8) << elapsed_secs2;
         ofile1 << setw(15) << setprecision(8) << elapsed_secs3;
+        ofile1 << setw(15) << setprecision(8) << elapsed_secs4;
     }
 
-    int alldata = atoi(argv[2]);
 
-    if(alldata < 0) {
-        //In case we want all data, we give a number less than 0 as the second command line
-        //argument.
+
+    if(argc >= 3) {
+        //If we want to write the computed solution, and exact solution to file
+        //provide a second output file on the command line.
+
+        outfile2 = argv[3];
+        ofile2.open(outfile2, ios::out);
         output(computed_solution,exact,relative_error, n);
     }
 
